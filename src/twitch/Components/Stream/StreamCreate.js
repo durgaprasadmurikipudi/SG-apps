d7c1fb5e3e898ffab057c54f30187caf8cc7b2b1
Durga Prasad Murikipudi <durgaprasadmurikipudi@gmail.com>
2020-02-10 10:53:42 +0530

    creating necessary forms

--- src/twitch/Components/Stream/StreamCreate.js
+++ src/twitch/Components/Stream/StreamCreate.js
@@ -3,22 +3,44 @@ import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends Component {
 
-  renderInput(formProps) {
-    console.log(formProps.input)
-    return <input {...formProps.input} />
+  renderInput(formProps) {    
+    return (
+      <div className="field">
+        <label>{formProps.label}</label>
+        <input {...formProps.input} />
+        <div>{formProps.meta.error && formProps.meta.touched && formProps.meta.error}</div>
+      </div>
+
+    );
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues);
   }
 
   render() {
     return (
-      <form>
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} />
+      <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+        <Field name="title" label="title" component={this.renderInput} />
+        <Field name="description" label="description"  component={this.renderInput} />
+        <button type="submit" className="ui primary button">Submit</button>
       </form>
     )
   }
 }
 
 
+const validate = formValues => {
+  const errors = {};
+
+  if(!formValues.title) errors.title = 'You must enter a title';
+
+  if(!formValues.description) errors.description = 'You must enter a description';
+
+  return errors;
+}
+
 export default reduxForm({
-  form: 'StreamCreate'
+  form: 'StreamCreate',
+  validate
 })(StreamCreate);
