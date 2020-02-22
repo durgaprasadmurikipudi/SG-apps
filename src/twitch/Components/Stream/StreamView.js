4ea3595c75794d6ede3b10c51a18abaf9f67c1bb
Durga Prasad Murikipudi <durgaprasadmurikipudi@gmail.com>
2020-02-22 13:48:11 +0530

    Twitch app mini clone complete

--- src/twitch/Components/Stream/StreamView.js
+++ src/twitch/Components/Stream/StreamView.js
@@ -1,15 +1,42 @@
 import React from 'react'
 import { connect } from 'react-redux';
+import flv from 'flv.js';
 
 import { fetchStream } from '../../actions';
 
 class StreamView extends React.Component {
   constructor(props) {
     super(props);
+    this.videoStreamingRef = React.createRef();
   }
 
   componentDidMount() {
-    this.props.fetchStream(this.props.match.id);
+    this.props.fetchStream(this.props.match.params.id);
+    this.buildStream();
+  }
+
+  componentDidUpdate() {
+    this.buildStream();
+  }
+
+  componentWillUnmount() {
+    if(this.player) {
+      this.player.destroy();
+    } 
+  }
+
+  buildStream = () => {
+    if(this.player || !this.props.stream) 
+      return;
+
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${this.props.match.params.id}.flv`
+    });
+    if(this.videoStreamingRef.current) {
+      this.player.attachMediaElement(this.videoStreamingRef.current);
+      this.player.load();
+    }
   }
 
   render() {
@@ -17,6 +44,7 @@ class StreamView extends React.Component {
 
     return (
       <div>
+        <video ref={this.videoStreamingRef} controls style={{width: '100%'}} /> 
         <h1>{this.props.stream.title}</h1>
         <h5>{this.props.stream.description}</h5>
       </div>
